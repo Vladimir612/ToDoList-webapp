5e6102a2b134a7d1e3d5c2198b46cb4b3a4dd03a
Vladimir612 <vladimir.12.lazarevic@gmail.com>
2020-12-09 23:21:41 +0100

    Changed date

--- public/script.js
+++ public/script.js
@@ -26,25 +26,25 @@ const LINE_THROUGH = 'lineThrough';
 let date = document.querySelector('.date');
 let day;
 switch(new Date().getDay()){
-    case 0:
+    case 1:
         day = 'Monday';
         break;
-    case 1:
+    case 2:
         day = 'Tuesday';
         break;
-    case 2:
+    case 3:
         day = 'Wednesday';
         break;
-    case 3:
+    case 4:
         day = 'Thursday';
         break;
-    case 4:
+    case 5:
         day = 'Friday';
         break;
-    case 5:
+    case 6:
         day = 'Saturday';
         break;
-    case 6:
+    case 0:
         day = 'Sunday';
         break;
 }
